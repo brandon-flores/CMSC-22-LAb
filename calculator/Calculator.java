0fcb43189ba0fce76d3ec6b59994608d93ed3543
Brandon Flores <brandonjaylapaflores@gmail.com>
2016-11-02 10:31:32 +0800

    With Grid Layout and button colors

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -10,24 +10,21 @@ import java.math.BigDecimal;
  * Created by brandon on 10/28/2016.
  */
 public class Calculator extends JFrame{
-    JButton button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonplus, buttonminus, buttontimes, buttondivide, bpoint, bequals, clear, allclear;
-    JTextArea display;
-    BigDecimal a, b, answer;
-    int ope;
-    boolean flag = false;
-    boolean nega = true;
+    private JButton buttonplus, buttonminus, buttontimes, buttondivide, bpoint, bequals, clear, allclear;
+    private JTextArea display;
+    private JButton[] nums = new JButton[10];
+    private BigDecimal a, b, answer;
+    private JPanel buttons;
+    private int ope;
+    private boolean flag = false, nega = true;
 
     Calculator(){
-        button1 = new JButton("1");
-        button2 = new JButton("2");
-        button3 = new JButton("3");
-        button4 = new JButton("4");
-        button5 = new JButton("5");
-        button6 = new JButton("6");
-        button7 = new JButton("7");
-        button8 = new JButton("8");
-        button9 = new JButton("9");
-        button0 = new JButton("0");
+        Font f = new Font("Arial", Font.BOLD, 20);
+        for(int i = 0; i < 10; i++) {
+            nums[i] = new JButton("" + i);
+            nums[i].setBackground(Color.GRAY);
+            nums[i].setFont(f);
+        }
         bpoint = new JButton(".");
         bequals = new JButton("=");
         buttonplus = new JButton("+");
@@ -37,135 +34,92 @@ public class Calculator extends JFrame{
         clear = new JButton("Clear");
         allclear = new JButton("All Clear");
         display = new JTextArea();
-        Font f = new Font("Arial", Font.BOLD, 20);
+        buttons = new JPanel(new GridLayout(4, 4, 0, 0));
 
         display.setBounds(0, 0, 400, 100);
         display.setEditable(false);
         display.setFont(new Font("Times New Roman", Font.BOLD, 25));
-        //display.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        clear.setBounds(0, 100, 200, 75);
-        clear.setFont(f);
-        allclear.setBounds(200, 100, 200, 75);
-        allclear.setFont(f);
-        button7.setBounds(0, 175, 100, 75);
-        button7.setFont(f);
-        button8.setBounds(100, 175, 100, 75);
-        button8.setFont(f);
-        button9.setBounds(200, 175, 100, 75);
-        button9.setFont(f);
-        buttonplus.setBounds(300, 175, 100, 75);
-        buttonplus.setFont(f);
-        button4.setBounds(0, 250, 100, 75);
-        button4.setFont(f);
-        button5.setBounds(100, 250, 100, 75);
-        button5.setFont(f);
-        button6.setBounds(200, 250, 100, 75);
-        button6.setFont(f);
-        buttonminus.setBounds(300, 250, 100, 75);
-        buttonminus.setFont(f);
-        button1.setBounds(0, 325, 100, 75);
-        button1.setFont(f);
-        button2.setBounds(100, 325, 100, 75);
-        button2.setFont(f);
-        button3.setBounds(200, 325, 100, 75);
-        button3.setFont(f);
-        buttontimes.setBounds(300, 325, 100, 75);
-        buttontimes.setFont(f);
-        bpoint.setBounds(0, 400, 100, 75);
-        bpoint.setFont(f);
-        button0.setBounds(100, 400, 100, 75);
-        button0.setFont(f);
-        bequals.setBounds(200, 400, 100, 75);
-        bequals.setFont(f);
-        buttondivide.setBounds(300, 400, 100, 75);
-        buttondivide.setFont(f);
+        display.setBackground(Color.LIGHT_GRAY);
 
-        button1.addActionListener(new MyActionListener());
-        button2.addActionListener(new MyActionListener());
-        button3.addActionListener(new MyActionListener());
-        button4.addActionListener(new MyActionListener());
-        button5.addActionListener(new MyActionListener());
-        button6.addActionListener(new MyActionListener());
-        button7.addActionListener(new MyActionListener());
-        button8.addActionListener(new MyActionListener());
-        button9.addActionListener(new MyActionListener());
-        button0.addActionListener(new MyActionListener());
-        buttonplus.addActionListener(new MyActionListener());
-        buttonminus.addActionListener(new MyActionListener());
-        buttontimes.addActionListener(new MyActionListener());
-        buttondivide.addActionListener(new MyActionListener());
-        bequals.addActionListener(new MyActionListener());
-        bpoint.addActionListener(new MyActionListener());
-        clear.addActionListener(new MyActionListener());
-        allclear.addActionListener(new MyActionListener());
+        clear.setFont(f);       clear.setBounds(0, 100, 200, 75);       clear.setForeground(Color.WHITE);
+        allclear.setFont(f);    allclear.setBounds(200, 100, 200, 75);  allclear.setForeground(Color.WHITE);
+        clear.setBackground(Color.DARK_GRAY);       allclear.setBackground(Color.DARK_GRAY);
+
+        buttonplus.setFont(f);      buttonplus.setForeground(Color.WHITE);      buttonplus.setBackground(Color.DARK_GRAY);
+        buttonminus.setFont(f);     buttonminus.setForeground(Color.WHITE);     buttonminus.setBackground(Color.DARK_GRAY);
+        buttontimes.setFont(f);     buttontimes.setForeground(Color.WHITE);     buttontimes.setBackground(Color.DARK_GRAY);
+        bpoint.setFont(f);          bpoint.setForeground(Color.WHITE);          bpoint.setBackground(Color.DARK_GRAY);
+        bequals.setFont(f);         bequals.setForeground(Color.WHITE);         bequals.setBackground(Color.DARK_GRAY);
+        buttondivide.setFont(f);    buttondivide.setForeground(Color.WHITE);    buttondivide.setBackground(Color.DARK_GRAY);
+
+        buttons.add(nums[7]);   buttons.add(nums[8]);   buttons.add(nums[9]);   buttons.add(buttonplus);
+        buttons.add(nums[4]);   buttons.add(nums[5]);   buttons.add(nums[6]);   buttons.add(buttonminus);
+        buttons.add(nums[1]);   buttons.add(nums[2]);   buttons.add(nums[3]);   buttons.add(buttontimes);
+        buttons.add(nums[0]);   buttons.add(bpoint);    buttons.add(bequals);   buttons.add(buttondivide);
+        buttons.setBounds(0, 175, 400, 300);
+
+        nums[1].addActionListener(new MyActionListener());      nums[2].addActionListener(new MyActionListener());
+        nums[3].addActionListener(new MyActionListener());      nums[4].addActionListener(new MyActionListener());
+        nums[5].addActionListener(new MyActionListener());      nums[6].addActionListener(new MyActionListener());
+        nums[7].addActionListener(new MyActionListener());      nums[8].addActionListener(new MyActionListener());
+        nums[9].addActionListener(new MyActionListener());      nums[0].addActionListener(new MyActionListener());
+        buttonplus.addActionListener(new MyActionListener());   buttonminus.addActionListener(new MyActionListener());
+        buttontimes.addActionListener(new MyActionListener());  buttondivide.addActionListener(new MyActionListener());
+        bequals.addActionListener(new MyActionListener());      bpoint.addActionListener(new MyActionListener());
+        clear.addActionListener(new MyActionListener());        allclear.addActionListener(new MyActionListener());
 
         add(display);
-        add(clear);
-        add(allclear);
-        add(button7);
-        add(button8);
-        add(button9);
-        add(button4);
-        add(button5);
-        add(button6);
-        add(button1);
-        add(button2);
-        add(button3);
-        add(bpoint);
-        add(button0);
-        add(bequals);
-        add(buttonplus);
-        add(buttonminus);
-        add(buttontimes);
-        add(buttondivide);
+        add(clear); add(allclear);
+        add(buttons);
 
         this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Calculator");
-        this.setSize(416,514);
+        this.setSize(403,505);
         this.setVisible(true);
+        this.setResizable(false);
     }
 
     class MyActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if(e.getSource() == button1){
+            if(e.getSource() == nums[1]){
                 change();
                 display.setText(display.getText() + "1");
             }
-            if(e.getSource() == button2) {
+            if(e.getSource() == nums[2]) {
                 change();
                 display.setText(display.getText() + "2");
             }
-            if(e.getSource() == button3) {
+            if(e.getSource() == nums[3]) {
                 change();
                 display.setText(display.getText() + "3");
             }
-            if(e.getSource() == button4) {
+            if(e.getSource() == nums[4]) {
                 change();
                 display.setText(display.getText() + "4");
             }
-            if(e.getSource() == button5) {
+            if(e.getSource() == nums[5]) {
                 change();
                 display.setText(display.getText() + "5");
             }
-            if(e.getSource() == button6) {
+            if(e.getSource() == nums[6]) {
                 change();
                 display.setText(display.getText() + "6");
             }
-            if(e.getSource() == button7) {
+            if(e.getSource() == nums[7]) {
                 change();
                 display.setText(display.getText() + "7");
             }
-            if(e.getSource() == button8) {
+            if(e.getSource() == nums[8]) {
                 change();
                 display.setText(display.getText() + "8");
             }
-            if(e.getSource() == button9) {
+            if(e.getSource() == nums[9]) {
                 change();
                 display.setText(display.getText() + "9");
             }
-            if(e.getSource() == button0) {
+            if(e.getSource() == nums[0]) {
                 change();
                 display.setText(display.getText() + "0");
             }
@@ -229,7 +183,7 @@ public class Calculator extends JFrame{
         }
     }
 
-    public void change(){
+    private void change(){
         if(flag){
             flag = false;
             display.setText("");
@@ -239,5 +193,4 @@ public class Calculator extends JFrame{
     public static void main(String[] args) {
         new Calculator();
     }
-
 }
